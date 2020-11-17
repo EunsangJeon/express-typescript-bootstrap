8832bc4d1ca75f2a50ca4859542c5df82513e3a5
EunsangJeon <eunsang.jeon.127@gmail.com>
2020-11-17 01:28:06 +0000

    done with the boilerplate parts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 import express from 'express';
+
 import { router } from './routes/loginRoutes';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['someString'] }));
 app.use(router);
 
 app.listen(3000, () => {
